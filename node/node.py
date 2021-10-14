c58e12b59e393e139a03594929a039cb11e8a059
Owen Krussow <okrussow@gmail.com>
2021-10-13 21:56:08 -0500

    started wallet

--- node/node.py
+++ node/node.py
@@ -7,7 +7,7 @@ import Crypto
 from Crypto.PublicKey import RSA
 from Crypto import Random
 import base64
-
+import rsa
 
 #each "block" = block number, nonce, data, prevhash, hash
 blockchain = [[0, 0, [], "0"*64, hashlib.sha256(("0" + "0" + "" + "0" * 64).encode("utf8")).hexdigest()]]
@@ -18,28 +18,7 @@ pending_transactions = []
 #====================================================================
 #   TODO This part needs to be figured out
 #        Keep in mind, the public crypto address is the computed public key, hashed
-
-def rsakeys():  
-    length=1024  
-    privatekey = RSA.generate(length, Random.new().read)  
-    publickey = privatekey.publickey()  
-    return privatekey, publickey
-
-def encrypt(rsa_publickey,plain_text):
-    cipher_text=rsa_publickey.encrypt(plain_text,32)[0]
-    b64cipher=base64.b64encode(cipher_text)
-    return b64cipher
-
-def decrypt(rsa_privatekey,b64cipher):
-    decoded_ciphertext = base64.b64decode(b64cipher)
-    plaintext = rsa_privatekey.decrypt(decoded_ciphertext)
-    return plaintext
-
-def sign(privatekey,data):
-    return base64.b64encode(str((privatekey.sign(data,''))[0]).encode())
-
-def verify(publickey,data,sign):
-    return publickey.verify(data,(int(base64.b64decode(sign)),))
+#   This part has been replaced by rsa.py
 
 #======================================================================
 
--- wallet/wallet.py
+++ wallet/wallet.py
@@ -1,25 +1,50 @@
 import rsa
+import json
 from base64 import b64encode, b64decode
 
-msg1 = "Hello Tony, I am Jarvis!"
-msg2 = "Hello Toni, I am Jarvis!"
-keysize = 2048
-(public, private) = rsa.newkeys(keysize)
-encrypted = b64encode(rsa.encrypt(msg1.encode("utf8"), public))
-decrypted = rsa.decrypt(b64decode(encrypted), private)
-signature = b64encode(rsa.sign(msg1.encode("utf8"), private, "SHA-512"))
-verify = rsa.verify(msg1.encode("utf8"), b64decode(signature), public)
+user_wallet = []
 
-print(private.exportKey('PEM'))
-print()
-print(public.exportKey('PEM'))
-print()
-print("Encrypted: " + encrypted.decode("utf8"))
-print()
-print("Decrypted: '%s'" % decrypted.decode("utf8"))
-print()
-print("Signature: " + signature.decode("utf8"))
-print()
-print("Verify: %s" % verify)
-print()
-rsa.verify(msg2.encode("utf8"), b64decode(signature), public)
+
+
+
+def main():
+    try:
+        with open("wallet.file", "r") as f:
+            user_wallet = json.load(f)
+    except FileNotFoundError:
+        print("Wallet file not found, will create new one")
+        with open("wallet.file", "w+") as f:
+            f.write(json.dumps(user_wallet))
+
+    while True:
+        user_choice = input("(S)END, (R)ECIEVED, (H)ISTORY: ").upper()
+        if user_choice == "S":
+            continue
+
+if "__name__" == __main__:
+    main()
+
+
+
+#msg1 = "Hello Tony, I am Jarvis!"
+#msg2 = "Hello Toni, I am Jarvis!"
+#keysize = 2048
+#(public, private) = rsa.newkeys(keysize)
+#encrypted = b64encode(rsa.encrypt(msg1.encode("utf8"), public))
+#decrypted = rsa.decrypt(b64decode(encrypted), private)
+#signature = b64encode(rsa.sign(msg1.encode("utf8"), private, "SHA-512"))
+#verify = rsa.verify(msg1.encode("utf8"), b64decode(signature), public)
+#
+#print(private.exportKey('PEM'))
+#print()
+#print(public.exportKey('PEM'))
+#print()
+#print("Encrypted: " + encrypted.decode("utf8"))
+#print()
+#print("Decrypted: '%s'" % decrypted.decode("utf8"))
+#print()
+#print("Signature: " + signature.decode("utf8"))
+#print()
+#print("Verify: %s" % verify)
+#print()
+#rsa.verify(msg2.encode("utf8"), b64decode(signature), public)
